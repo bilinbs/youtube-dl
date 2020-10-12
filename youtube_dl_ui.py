1e1f2148fd8a63834e6f3c2b1d343af5a2b9b4a4
bilinbs <bilinbs@gmail.com>
2020-10-12 21:57:22 +0530

    Adding progressbar

--- youtube_dl_ui.py
+++ youtube_dl_ui.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 import youtube_dl
 import tkinter as tk
 import tkinter.ttk as ttk
-import tkinter.messagebox 
+import tkinter.messagebox as messagebox
 
 class FormatLogger(object):
     message = ''
@@ -17,20 +17,12 @@ class FormatLogger(object):
 
 window = tk.Tk()
 window.title("YoutubeDL")
-
-format_sel = ''
-formats_checkbox_val = tk.StringVar()
 fmtListBox_vid = tk.Listbox()
 fmtListBox_aud = tk.Listbox()
 fmtListBox_av = tk.Listbox()
 url = ''
 dlProgressBar = ttk.Progressbar()
-rowNumWin = 0
-
-def formatChanged():
-    global format_sel
-    print(format_sel)
-    print(formats_checkbox_val)
+dlLabel = tk.Label()
 
 def progress_display(d):
     global dlProgressBar
@@ -41,21 +33,38 @@ def progress_display(d):
         precentValue = d['_percent_str'].strip('%')
         print("ivide" + precentValue)
         dlProgressBar['value'] = precentValue
+        dlLabel['text'] = 'Downloading selected formats: ' + format_selected + ' @' + d['_speed_str'] + ' ETA:'+ d['_eta_str']
         window.update()
-    elif d['status'] == 'downloaded':
-        messagebox.showinfo('Dowload Complete', repr(d))
+    elif d['status'] == 'finished':
+        messagebox.showinfo('Dowload Complete', 'Downloaded ' + d['_total_bytes_str'] + ' to \n' + d['filename'])
+        dlLabel['text'] = 'Completed downloading ' +  d['_total_bytes_str']
+
+def getSelectionListBox(listbox, formats, rowNum):
+    listbox.destroy()
+    listbox = tk.Listbox(height = len(formats), selectmode='browse', exportselection=0)
+    listbox.grid(row = rowNum, column=0, columnspan = 3, sticky="ew")
+    return listbox
+
+def insertToListBox(formats, listbox):
+    rowNum = 0
+    for fmt in formats:
+        listbox.insert(rowNum, fmt)
+        rowNum = rowNum + 1
 
 def list_formats():
     global url
+    global formats
+    global formats_checkbox_val
+    global fmtListBox
+    global dlButton
+    global fmtListBox_vid
+    global fmtListBox_aud
+    global fmtListBox_av
     url=[urlEntry.get().strip()]
     fmtLog = FormatLogger()
     ytdl_opts = { 'listformats' : 'true' , 'logger': fmtLog}
     with youtube_dl.YoutubeDL(ytdl_opts) as ydl:
         format_list = ydl.download(url)
-    global formats
-    global formats_checkbox_val
-    global fmtListBox
-    global dlButton
     formats = fmtLog.message.split('\n')
     displayText = 'If no formats from below are selected, the best available will be downlaoded \n' + formats.pop(0) + '\n' + '\t'.join(formats.pop(0).split())
     displayMessage = tk.Label(text= displayText, justify='left')
@@ -65,35 +74,28 @@ def list_formats():
     avFormats = list(filter(lambda x: not ('audio only' in x or 'video only' in x), formats))
     label_vid = tk.Label(text='Video only formats: (select one)')
     label_vid.grid(row = 2, column=0, columnspan = 3, sticky="ew")
-    fmtListBox_vid = tk.Listbox(height = len(videoOnlyFormats), selectmode='browse')
-    fmtListBox_vid.grid(row = 3, column=0, columnspan = 3, sticky="ew")
+    fmtListBox_vid = getSelectionListBox(fmtListBox_vid, videoOnlyFormats, 3)
     label_aud = tk.Label(text='Audio only formats: (select one)')
     label_aud.grid(row = 4, column=0, columnspan = 3, sticky="ew")
-    fmtListBox_aud = tk.Listbox(height = len(audioOnlyFormats), selectmode='browse')
-    fmtListBox_aud.grid(row = 5, column=0, columnspan = 3, sticky="ew")
+    fmtListBox_aud = getSelectionListBox(fmtListBox_aud, audioOnlyFormats, 5)
     label_av = tk.Label(text='Audio&Video formats: If one of the below formats are selected, choices made above will be ignored')
     label_av.grid(row = 6, column=0, columnspan = 3, sticky="ew")
-    fmtListBox_av = tk.Listbox(height = len(audioOnlyFormats), selectmode='browse')
-    fmtListBox_av.grid(row = 7, column=0, columnspan = 3, sticky="ew")
-    rowNum = 0
-    for fmt in videoOnlyFormats:
-        fmtListBox_vid.insert(rowNum, fmt)
-        rowNum = rowNum + 1
-    rowNum = 0    
-    for fmt in audioOnlyFormats:
-        fmtListBox_aud.insert(rowNum, fmt)
-        rowNum = rowNum + 1
-    rowNum = 0    
-    for fmt in avFormats:
-        fmtListBox_av.insert(rowNum, fmt)
-        rowNum = rowNum + 1    
+    fmtListBox_av = getSelectionListBox(fmtListBox_av, avFormats,7)
+    insertToListBox(videoOnlyFormats, fmtListBox_vid)
+    insertToListBox(audioOnlyFormats, fmtListBox_aud)
+    insertToListBox(avFormats, fmtListBox_av) 
     dlButton.config(state='normal')
 
 def downloadSelection():
-    global fmtListBox
     global rowNumWin
     global window
     global dlProgressBar
+    global dlLabel
+    global format_selected
+    global url
+    global fmtListBox_vid
+    global fmtListBox_aud
+    global fmtListBox_av
     selectedVideo = fmtListBox_vid.curselection()
     selectedAudio = fmtListBox_aud.curselection()
     selectedAV = fmtListBox_av.curselection()
@@ -106,17 +108,18 @@ def downloadSelection():
         format_selected = ''
         if selectedVideo != ():
             format_selected = fmtListBox_vid.get(selectedVideo).split()[0]
+            print(selectedVideo)
         if selectedAudio != ():
             if format_selected != '':
                 format_selected = format_selected + '+'
-            format_selected = format_selected + selectedAudio.get(selectedAudio).split()[0]
+            format_selected = format_selected + fmtListBox_aud.get(selectedAudio).split()[0]
             
-        #cur_select = []
-        #for sel in selected:
-        #    cur_select.append(fmtListBox.get(sel).split()[0])
-        #format_selected = '+'.join(str(sel) for sel in cur_select)
+    separator = ttk.Separator(window)
+    separator.grid(row = 8, column=0, columnspan = 4, sticky="ew")    
     dlProgressBar = ttk.Progressbar(window, orient='horizontal', mode='determinate', maximum='100', value='0')
-    dlProgressBar.grid(row = 8, column = 1, columnspan = 2, sticky="ew")    
+    dlProgressBar.grid(row = 9, column = 1, columnspan = 2, sticky="ew")    
+    dlLabel = tk.Label(window, text='Downloading selected formats: ' + format_selected)
+    dlLabel.grid(row = 9, column = 0, sticky="ew")
     ydl_opts = {
         'format': format_selected,
         'progress_hooks': [progress_display]
@@ -124,12 +127,6 @@ def downloadSelection():
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         format_list = ydl.download(url)
     
-    
-
-
-#window.rowconfigure(0, minsize=640, weight=1)
-#window.columnconfigure(0, minsize=480, weight=1)
-#window.columnconfigure(1, minsize=80, weight=1)
 label_dl = tk.Label(window, text = 'Enter the video url:')
 urlEntry = tk.Entry(window,width = 50)
 listButton = tk.Button(window, text="List Formats", command=list_formats)
@@ -138,6 +135,4 @@ label_dl.grid(row = 0, column=0, sticky = "nw", padx=5)
 urlEntry.grid(row=0, column=1, sticky="nw", padx=5, pady=5)
 listButton.grid(row=0, column=2, sticky="nw", padx=5)
 dlButton.grid(row=0, column=3, sticky="nw", padx=5)
-#urlEntry.pack()
-#dlButton.pack()
 window.mainloop()
